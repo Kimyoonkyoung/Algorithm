d584b0918610554afe2aa746a2f9123e7bd03655
rachel.minii <rachel.minii@kakaocorp.com>
2018-06-19 19:22:34 +0900

    [TEST - solution] 정답!
    while 문 한번에 한바퀴로 생각하고,
    right, down, left, up  지정해주기

---
+++ InterviewBit/Arrays/SpiralOrderMatrix1.cpp
@@ -0,0 +1,113 @@
+//
+// Created by yoonkyoung on 2018. 6. 18..
+//
+
+#include <climits>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+struct Point
+{
+    int x;
+    int y;
+};
+
+vector<int> spiralOrder(const vector<vector<int> > &A) {
+
+    std::vector<int> spiral;
+
+    int width = (int)A[0].size();
+    int height = (int)A.size();
+
+    if (width == 1 && height == 1) {
+        spiral.push_back(A[0][0]);
+        return spiral;
+    }
+
+    bool visited[height][width];
+    std::memset(visited, false, sizeof(visited));
+
+    Point right, left, down, up;
+    right.x = 0; right.y = 0;
+    left.x = width-2; left.y = height-1;
+    down.x = width-1; down.y = 1;
+    up.x = 0; up.y = height-2;
+
+    while (right.y < (height>>1) && left.y >= (height>>1)
+           && down.x >= (width>>1) && up.x < (width>>1)) {
+
+        // right 전진
+        int initX = right.x;
+        int initY = right.y;
+        while (right.x < width && !visited[right.y][right.x]) {
+            spiral.push_back(A[right.y][right.x]);
+            visited[right.y][right.x] = true;
+            right.x++;
+        }
+        right.x = initX + 1;
+        right.y = initY + 1;
+
+        // down 전진
+        initX = down.x;
+        initY = down.y;
+        while (down.y < height && !visited[down.y][down.x]) {
+            spiral.push_back(A[down.y][down.x]);
+            visited[down.y][down.x] = true;
+            down.y++;
+        }
+        down.x = initX - 1;
+        down.y = initY + 1;
+
+        // left 전진
+        initX = left.x;
+        initY = left.y;
+        while (left.x >= 0 && !visited[left.y][left.x]) {
+            spiral.push_back(A[left.y][left.x]);
+            visited[left.y][left.x] = true;
+            left.x--;
+        }
+        left.x = initX - 1;
+        left.y = initY - 1;
+
+        // up 전진
+        initX = up.x;
+        initY = up.y;
+        while (up.y > 0 && !visited[up.y][up.x]) {
+            spiral.push_back(A[up.y][up.x]);
+            visited[up.y][up.x] = true;
+            up.y--;
+        }
+        up.x = initX + 1;
+        up.y = initY - 1;
+    }
+
+    return spiral;
+
+}
+
+
+int main() {
+    vector<vector<int>> A;
+    vector<int> sub;
+
+    sub.push_back(1);
+    sub.push_back(2);
+    A.push_back(sub);
+
+    sub.clear();
+    sub.push_back(3);
+    sub.push_back(4);
+    A.push_back(sub);
+
+    sub.clear();
+    sub.push_back(5);
+    sub.push_back(6);
+    A.push_back(sub);
+
+    vector<int> ret = spiralOrder(A);
+    for (int i = 0; i < ret.size(); i++)
+        printf("%d, ", ret[i]);
+
+}
