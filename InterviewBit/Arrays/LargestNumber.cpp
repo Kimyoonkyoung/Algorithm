4660b2b20f8f26898871395d3928c3be11cddeab
rachel.minii <rachel.minii@kakaocorp.com>
2018-06-21 20:27:54 +0900

    [Add] unit 으로 관리하고싶은 알고리즘을 쓰기위한 Self 폴더 추가 및 merge sort

---
+++ InterviewBit/Arrays/LargestNumber.cpp
@@ -0,0 +1,87 @@
+/**
+ * Created by Rachel.minii 
+ * Date : 2018. 6. 21.
+ * Project : Algorithm
+ */
+
+#include <climits>
+#include <vector>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void merge(vector<int>& A, int startIdx, int midIdx, int endIdx) {
+    vector<int> tmp;
+
+    int l = startIdx;
+    int r = midIdx + 1;
+
+    while (l <= midIdx && r <= endIdx) {
+        if (A[l] <= A[r]) {
+            tmp.push_back(A[l]);
+            l++;
+        } else {
+            tmp.push_back(A[r]);
+            r++;
+        }
+    }
+
+    if (l <= midIdx) {
+        for (int i = l; i <= midIdx; i++) {
+            tmp.push_back(A[i]);
+        }
+    }
+    if (r <= endIdx) {
+        for (int i = r; i <= endIdx; i++) {
+            tmp.push_back(A[r]);
+        }
+    }
+
+    for (int i = startIdx; i <= endIdx; i++) {
+        A[i] = tmp[i - startIdx];
+    }
+}
+
+
+void mergesort(vector<int>& A, int startIdx, int endIdx) {
+    int mid;
+
+    // 종료조건
+    if (A.size() == 1)
+        return;
+
+    if (startIdx < endIdx) {
+        mid = (startIdx + endIdx) / 2;
+        mergesort(A, startIdx, mid);
+        mergesort(A, mid + 1, endIdx);
+        merge(A, startIdx, mid, endIdx);
+    }
+}
+
+
+
+string largestNumber(vector<int>& A) {
+    mergesort(A, 0, A.size()-1);
+
+    string ret;
+    for (int i = 0; i < A.size(); i++) {
+        ret.append(to_string(A[i]));
+    }
+
+    return ret;
+}
+
+
+int main() {
+    vector<int> A;
+
+    A.push_back(3);
+    A.push_back(30);
+    A.push_back(34);
+    A.push_back(5);
+    A.push_back(9);
+
+    string ret = largestNumber(A);
+    printf("%s\n", ret.c_str());
+}
