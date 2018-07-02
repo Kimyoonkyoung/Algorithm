5b6934c6b714920ef8c278017dd90ea036755216
rachel.minii <rachel.minii@kakaocorp.com>
2018-07-02 16:59:40 +0900

    [SOLVED -2] Light weight 추가

--- InterviewBit/Math/GreatestCommonDivisor.cpp
+++ InterviewBit/Math/GreatestCommonDivisor.cpp
@@ -17,18 +17,38 @@ int gcd(int A, int B) {
     if (B == 0)
         return A;
 
+#ifdef EDITORIAL
     int smallVal = std::min(A, B);
     for (int i = smallVal; i > 1; i--) {
         if (A % i == 0 && B % i == 0)
             return i;
     }
-
     return 1;
+#endif // EDITORIAL
+
+#ifndef LIGHTWEIGHT
+    /**
+     * 나눈 나머지를 계속 나눠가는 방법으로 접근
+     */
+    if(A < B) {
+        int temp = A;
+        A = B;
+        B = temp;
+    }
+    while(B != 0) {
+        int temp = B;
+        B = A % B;
+        A = temp;
+    }
+    return A;
+
+#endif // LIGHTWEIGHT
+
 }
 
 
 int main() {
-    printf("%d", gcd(2, 2));
+    printf("%d", gcd(18, 8));
 
     return 0;
 }
